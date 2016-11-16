00e73ddb6c86defca99934acb4003a4fbf3c9452
akopylov89 <cilicium_2@mail.ru>
2016-11-16 19:08:24 +0300

    Update script.py

--- script.py
+++ script.py
@@ -163,9 +163,6 @@ if __name__ == '__main__':
     for i in xrange(0, len(Car.all_cars)):
         p = threading.Thread(target=at_finish, args=[i, ])
         p.start()
-        
-    import sys
-    sys.exit(7)
 
 
 
